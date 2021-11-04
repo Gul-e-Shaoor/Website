e278b8b3a4977601cf49c000db6fdc9c39d94c20
Syed_Sibteali_Baqar <sibteali786@gmail.com>
2021-11-04 16:39:58 +0500

    changed hero buttons position and fixed navbar toggler in mobile size

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import { Fragment } from "react";
 import { Link } from "react-router-dom";
 import styles from "../Styles/Hero.module.css";
@@ -10,6 +10,21 @@ const Hero = () => {
           <div className="d-grid" style={{ height: "80%" }}>
             <div className="row justify-content-between align-items-center">
               <div className="col-sm-12 col-lg-6 d-flex flex-column ">
+                <div className="py-3">
+                  <p className="fw-bold h2 text-light">
+                    Empowering youth through capacity building{" "}
+                  </p>
+                  <p
+                    className="fs-6 text-light py-3 "
+                    style={{ opacity: "0.8" }}
+                  >
+                    We nurture young minds towards a productive approach and
+                    build their capacity for career development over
+                    international standards by fulfilling their need of
+                    counselling, mentorship, information, healthy entertainment
+                    and a supporting community.
+                  </p>
+                </div>
                 <div className="d-flex justify-content-start align-content-center">
                   <Link
                     to="/careers"
@@ -26,21 +41,6 @@ const Hero = () => {
                   >
                     <i className="fa fa-sign-in mx-2"></i> Register
                   </Link>
-                </div>
-                <div className="py-3">
-                  <p className="fw-bold h2 text-light">
-                    Empowering youth through capacity building{" "}
-                  </p>
-                  <p
-                    className="fs-6 text-light py-3 "
-                    style={{ opacity: "0.8" }}
-                  >
-                    We nurture young minds towards a productive approach and
-                    build their capacity for career development over
-                    international standards by fulfilling their need of
-                    counselling, mentorship, information, healthy entertainment
-                    and a supporting community.
-                  </p>
                 </div>
               </div>
               <div className="col-sm-12 col-lg-6">
